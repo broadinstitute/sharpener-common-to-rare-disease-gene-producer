624e7878742b8f8738015ab20ea775df5d0189bd
vdancik <vdancik@broadinstitute.org>
2019-12-12 15:34:52 -0500

    implement transformer

--- python-flask-server/swagger_server/controllers/transformer_controller.py
+++ python-flask-server/swagger_server/controllers/transformer_controller.py
@@ -7,6 +7,8 @@ from swagger_server.models.transformer_info import TransformerInfo  # noqa: E501
 from swagger_server.models.transformer_query import TransformerQuery  # noqa: E501
 from swagger_server import util
 
+from swagger_server.controllers.transformer import transformer_info
+from swagger_server.controllers.transformer import transform
 
 def transform_post(query):  # noqa: E501
     """transform_post
@@ -20,7 +22,7 @@ def transform_post(query):  # noqa: E501
     """
     if connexion.request.is_json:
         query = TransformerQuery.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    return transform(query)
 
 
 def transformer_info_get():  # noqa: E501
@@ -31,4 +33,4 @@ def transformer_info_get():  # noqa: E501
 
     :rtype: TransformerInfo
     """
-    return 'do some magic!'
+    return transformer_info()
